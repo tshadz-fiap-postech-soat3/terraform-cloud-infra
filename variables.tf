4660a55e2a4d051db1579cb54ebca5f92141e4fd
Thaís Matoso Sombra <thais.sombra@vizir.com.br>
2024-03-17 19:05:03 -0300

    fix: fix database url

---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "DB_REGION" {
+  description = "Database region"
+}
+
+variable "DB_TIER" {
+  description = "Database tier"
+}
+
+variable "GCP_ID" {
+  description = "Project ID"
+}
+
+variable "MYSQL_HOST" {
+  description = "DB Host"
+}
+
+variable "MYSQL_ROOT_PASSWORD" {
+  description = "DB Password"
+}
+
+variable "MYSQL_ROOT_USER" {
+  default = "root"
+}
+
+variable "MYSQL_DATABASE" {
+  default = "fastfood"
+}
+
+variable "MYSQL_USER" {
+  description = "DB Instance Name"
+}
+
+variable "MYSQL_PASSWORD" {
+  description = "DB Password"
+}
+
+variable "MYSQL_PORT" {
+  description = "DB Port"
+}
+
+variable "CLOUD_INSTANCE" {
+  description = "DB Instance Name"
+}
