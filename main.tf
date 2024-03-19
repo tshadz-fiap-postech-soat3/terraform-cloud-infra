dac855754bce0fffb0f65e2d68fe01d64e5c3cf2
Thaís Matoso Sombra <thais.sombra@vizir.com.br>
2024-03-18 23:05:08 -0300

    add resources

--- main.tf
+++ main.tf
@@ -16,6 +16,24 @@ terraform {
   }
 }
 
+# Enable Secret Manager API
+resource "google_project_service" "secretmanager_api" {
+  service            = "secretmanager.googleapis.com"
+  disable_on_destroy = false
+}
+
+# Enable SQL Admin API
+resource "google_project_service" "sqladmin_api" {
+  service            = "sqladmin.googleapis.com"
+  disable_on_destroy = false
+}
+
+# Enable Cloud Run API
+resource "google_project_service" "cloudrun_api" {
+  service            = "run.googleapis.com"
+  disable_on_destroy = false
+}
+
 provider "google" {
   project     = var.GCP_ID
   region      = var.REGION
